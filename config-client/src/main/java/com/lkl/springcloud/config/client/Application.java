2e1923ee3bcac2277ad0c687113fd6c297f9bc6a
liaokailin <51liaokailin@gmail.com>
2016-04-29 15:51:05 +0800

    dynamic modify config

--- config-client/src/main/java/com/lkl/springcloud/config/client/Application.java
+++ config-client/src/main/java/com/lkl/springcloud/config/client/Application.java
@@ -3,6 +3,7 @@ package com.lkl.springcloud.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 @ComponentScan
 @RestController
+@RefreshScope
 public class Application {
 
     @Value("${name:World!}") String name ;
