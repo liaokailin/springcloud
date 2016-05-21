ec4ef0a3105f17fd89306b528d6b0791a974ca7b
liaokailin <51liaokailin@gmail.com>
2016-05-21 23:09:32 +0800

    zuul

---
+++ zuul/src/main/java/com/lkl/springcloud/zuul/Application.java
@@ -0,0 +1,13 @@
+package com.lkl.springcloud.zuul;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
