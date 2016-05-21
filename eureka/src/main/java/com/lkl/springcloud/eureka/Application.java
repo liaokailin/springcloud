18447e96d953650a0b7e025cd23d98c35d4d0270
liaokailin <51liaokailin@gmail.com>
2016-05-21 23:04:38 +0800

    zuul

--- eureka/src/main/java/com/lkl/springcloud/eureka/Application.java
+++ eureka/src/main/java/com/lkl/springcloud/eureka/Application.java
@@ -4,8 +4,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
